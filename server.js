6227451904ddb3ae3d7a5dbff40a9223cf18b0e1
woldak <jogu407@gmail.com>
2023-09-27 16:20:26 +0900

    db 연결 및 html 코드수정!

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 const express = require('express')
 const app = express()
 port=8080;
-app.listen(port,() => {
+
+app.use(express.static(__dirname + '/public'));
+
+
+const { MongoClient } = require('mongodb')
+
+let db
+const url = 'mongodb+srv://wxcv3123:Cka9Ts6rg8ISgH9w@cluster0.x18a4ei.mongodb.net/?retryWrites=true&w=majority'
+new MongoClient(url).connect().then((client)=>{
+  console.log('DB연결성공')
+  db = client.db('forum')
+  app.listen(port,() => {
     console.log(`http://localhost:${port} 에서 서버 실행중`)
 })
-app.get('/',(요청, 응답) =>{
-    응답.send('반갑습니다')
+}).catch((err)=>{
+  console.log(err)
 })
+
+app.get('/',(요청, 응답) =>{
+    응답.sendFile(__dirname+"/public/html/index.html")
+})
+app.get('/list', async (요청, 응답) => {
+   let result = await db.collection('post').find().toArray()
+   console.log(result[0].title)
+    응답.send('안녕')
+  })
